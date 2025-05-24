d6e9586b6864fc8e29454fc3ddeb1ea4260523e2
daraklhr <darakalhorx@gmail.com>
2025-05-24 05:28:48 +0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,7 @@
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open('darkside-cache-v1').then(cache => cache.addAll(['.', './index.html', './manifest.json'])));
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
